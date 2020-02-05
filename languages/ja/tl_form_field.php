5cf4c1c52a40dd323e2db9cb31da32f0f39608a6
Takahiro Kambe <taca@back-street.net>
2020-02-05 10:36:49 +0900

    Previous translations.

---
+++ languages/ja/tl_form_field.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * mp_forms extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2015-2016, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       https://github.com/terminal42/contao-mp_forms
+ */
+
+/**
+ * Form fields
+ */
+$GLOBALS['TL_LANG']['FFL']['mp_form_pageswitch'] = ['Page break', 'Separates the form fields into different pages/steps.'];
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_form_field']['mp_forms_backButton'] = ['Back button label', 'Please enter the label of the back button.'];
